52b58c9c81ca123e7d13b55d934f11bf61182ab9
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-23 10:18:23 +0530

    Create 1.c

---
+++ Basic/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    //10+20=30 10-20=-10 10*20=200 10/20=0 10%20=10
+    printf("%d\n",10+20);
+    printf("%d\n",10-20);
+    printf("%d\n",10*20);
+    printf("%d\n",10/20);
+    printf("%d\n",10%20);
+    return 0;
+}
