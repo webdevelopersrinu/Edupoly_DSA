820da8322259c4661ffd9f3ed012420538dfe4c3
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-25 10:18:32 +0530

    Create 14.c

---
+++ Basic/14.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+    float pi=3.14;
+    float radius = 20 ;
+    float circumference =2*(radius*pi);
+    printf("circumference of circle is %f", circumference);
+    return 0;
+}
