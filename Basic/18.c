b0327f37bb720a834c3d8fb2318c374764dd5b3b
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-25 10:36:28 +0530

    Create 18.c

---
+++ Basic/18.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    float student1=67;
+    float student2=48;
+    float student3=74;
+    float average=(student1+student2+student3)/3;
+    printf("average of students marks is %.2f", average);
+    return 0;
+}
