629cb558f2eaad24682baccdfb39c837b065d675
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-25 10:05:47 +0530

    Create 13.c

---
+++ Basic/13.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+    int length=50;
+    int breadth = 35 ;
+    int perimeter =2*(length + breadth);
+    printf("perimeter of rectangle is %d", perimeter);
+    return 0;
+}
