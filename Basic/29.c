f66a36b667fc974a4061504d43ca039789251e52
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-29 11:22:17 +0530

    Create 29.c

---
+++ Basic/29.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+   int length;
+   int breadth;
+   printf("enter a rectangle length :-");
+   scanf(" %d",&length);
+   printf("enter a rectangle breadth :-");
+   scanf(" %d",&breadth);
+   printf("area of rectangle %d",length*breadth);
+   return 0;
+}   
