18047a459b3c08fe828ac42beb91877ec54d47c6
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-29 11:18:42 +0530

    Create 28.c

---
+++ Basic/28.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+   int side;
+   printf("enter a square side :-");
+   scanf(" %d",&side);
+   printf("area of square %d",side*side);
+   return 0;
+}   
