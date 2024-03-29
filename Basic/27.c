e9032f6119b1b8a4eee4b1fb918efe7e1a732e20
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-29 10:57:31 +0530

    Create 27.c

---
+++ Basic/27.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+   int a=50;
+   int b=100;
+   int c=90;
+   a>b && a>c ? printf("a is big"): b>c ?printf("b is big"): printf("c is big");;
+   return 0;
+}   
