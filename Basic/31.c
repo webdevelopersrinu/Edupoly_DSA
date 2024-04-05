84572b6c5478617f7eb736a7ab554df2c50d8510
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-04-05 12:19:25 +0530

    Create 31.c

---
+++ Basic/31.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    float height;
+    float base ;
+    printf("enter triangle height : ");
+    scanf("%f",&height);
+    printf("enter triangle base : ");
+    scanf("%f",&base);
+    printf("area of the triangle is : %f",0.5*( height*base));
+    return 0;
+}
